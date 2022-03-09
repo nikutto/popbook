50a8670c23f197d48d186bfe347817886a9da0bd
Jumpei Mori <nikutto.kom@gmail.com>
2022-03-09 14:40:49 +0900

    Use postgres

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("mysql:mysql-connector-java")
+    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
