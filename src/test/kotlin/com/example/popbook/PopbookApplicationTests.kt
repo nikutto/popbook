add40a2690df1d38bcb9ad5e912b274e44253253
Jumpei Mori <nikutto.kom@gmail.com>
2022-03-09 14:10:07 +0900

    Fix detekted file

--- src/test/kotlin/com/example/popbook/PopbookApplicationTests.kt
+++ src/test/kotlin/com/example/popbook/PopbookApplicationTests.kt
@@ -2,11 +2,13 @@ package com.example.popbook
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.Suppress
 
 @SpringBootTest
 class PopbookApplicationTests {
 
 	@Test
+	@Suppress("EmptyFunctionBlock")
 	fun contextLoads() {
 	}
 
