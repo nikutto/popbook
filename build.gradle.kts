e8aa615a50cea27244cbffd9646159f9bcd5bf7b
Jumpei Mori <nikutto.kom@gmail.com>
2022-03-16 09:00:05 +0900

    Add mockk

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:1.12.3")
 }
 
 tasks.withType<KotlinCompile> {
