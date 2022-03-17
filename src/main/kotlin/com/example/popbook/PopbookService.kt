11e44368b4faca20d055b881344501d7b7cb7c87
Jumpei Mori <nikutto.kom@gmail.com>
2022-03-17 16:42:51 +0900

    refactor update func

--- src/main/kotlin/com/example/popbook/PopbookService.kt
+++ src/main/kotlin/com/example/popbook/PopbookService.kt
@@ -2,6 +2,7 @@ package com.example.popbook
 
 import com.example.popbook.api.RakutenAPIService
 import com.example.popbook.dao.Book
+import com.example.popbook.dto.Item
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -16,7 +17,11 @@ class PopbookService(
 
     fun listAll() = bookRepository.findAll().toList()
 
-    @Suppress("ComplexCondition")
+    private fun isValidItem(item: Item) = item.title != null &&
+        item.author != null &&
+        item.itemUrl != null &&
+        item.mediumImageUrl != null
+
     private fun insertUpdate() {
         val appId = System.getenv("APP_ID")!!
         val now = LocalDateTime.now()
@@ -25,19 +30,14 @@ class PopbookService(
             val items = books.Items
             for (elem in items) {
                 val item = elem.Item
-                if (item != null &&
-                    item.title != null &&
-                    item.author != null &&
-                    item.itemUrl != null &&
-                    item.mediumImageUrl != null
-                ) {
+                if (item != null && isValidItem(item)) {
                     val book = Book(
-                        null,
-                        item.title!!,
-                        item.author!!,
-                        item.itemUrl!!,
-                        item.mediumImageUrl!!,
-                        now,
+                        id = null,
+                        title = item.title!!,
+                        author = item.author!!,
+                        itemUrl = item.itemUrl!!,
+                        imageUrl = item.mediumImageUrl!!,
+                        createdAt = now,
                     )
                     bookRepository.save(book)
                 }
