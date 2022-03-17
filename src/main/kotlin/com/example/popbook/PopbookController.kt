f4c318e49806750de99a9cd1ff756af2acfb5e83
Jumpei Mori <nikutto.kom@gmail.com>
2022-03-17 17:43:01 +0900

    Change date format

--- src/main/kotlin/com/example/popbook/PopbookController.kt
+++ src/main/kotlin/com/example/popbook/PopbookController.kt
@@ -1,17 +1,33 @@
 package com.example.popbook
 
+import com.example.popbook.dao.Book
+import com.example.popbook.dto.BookDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import kotlin.Suppress
+import java.time.format.DateTimeFormatter
 
 @Controller
 class PopbookController(
     @Autowired val popbookService: PopbookService
 ) {
+    companion object {
+        private fun toDtos(books: List<Book>): List<BookDto> {
+            val df = DateTimeFormatter.ofPattern("yyyy-MM-dd kk:mm")
+            return books.map {
+                BookDto(
+                    title = it.title,
+                    author = it.author,
+                    itemUrl = it.itemUrl,
+                    imageUrl = it.imageUrl,
+                    createdAt = df.format(it.createdAt!!)
+                )
+            }
+        }
+    }
 
     @Suppress("FunctionOnlyReturningConstant")
     @GetMapping("/hello")
@@ -27,7 +43,7 @@ class PopbookController(
     @Suppress
     @GetMapping("/")
     fun popbook(model: Model): String {
-        model["books"] = popbookService.listPopbooks()
+        model["books"] = toDtos(popbookService.listPopbooks())
         return "list"
     }
 
@@ -36,7 +52,7 @@ class PopbookController(
     fun debug(model: Model): String {
         val msg = popbookService.debug()
         model["msg"] = "Book list is updated in $msg minutes."
-        model["books"] = popbookService.listAll()
+        model["books"] = toDtos(popbookService.listAll())
         return "debug"
     }
 }
---
+++ src/main/kotlin/com/example/popbook/dto/BookDto.kt
@@ -0,0 +1,9 @@
+package com.example.popbook.dto
+
+data class BookDto(
+    val title: String,
+    val author: String,
+    val itemUrl: String,
+    val imageUrl: String,
+    val createdAt: String
+)
