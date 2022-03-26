ad0c86c051826b42bec9de09d80b834a11cb6ccc
Jumpei Mori <nikutto.kom@gmail.com>
2022-03-26 10:47:46 +0900

    purify configs

--- local_script.sh
+++ local_script.sh
@@ -2,7 +2,9 @@ echo "Processing..."
 
 set -e
 export PGDATABASE="testdb"
-export PGURL="jdbc:postgresql://localhost:5432/${PGDATABASE}"
+export PGHOST="localhost"
+export PGPORT=5432
+export PGURL="jdbc:postgresql://${PGHOST}:${PGPORT}/${PGDATABASE}"
 export PGUSER="postgres"
 export PGPASSWORD="password"
 set +e
