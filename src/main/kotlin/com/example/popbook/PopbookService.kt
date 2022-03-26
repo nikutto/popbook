5058ef8041923dd3d3e997521ae35d4df1a5aeeb
Jumpei Mori <nikutto.kom@gmail.com>
2022-03-26 14:46:18 +0900

    Adjust api

--- src/main/kotlin/com/example/popbook/PopbookService.kt
+++ src/main/kotlin/com/example/popbook/PopbookService.kt
@@ -2,7 +2,6 @@ package com.example.popbook
 
 import com.example.popbook.api.RakutenAPIService
 import com.example.popbook.dao.Book
-import com.example.popbook.dto.Item
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -19,40 +18,30 @@ class PopbookService(
     fun listAll() = bookRepository.findAll().toList()
 
     companion object {
-        private fun isValidItem(item: Item) = item.title != null &&
-            item.author != null &&
-            item.itemUrl != null &&
-            item.mediumImageUrl != null &&
-            (item.booksGenreId == null || !item.booksGenreId!!.startsWith("001001"))
 
         private fun getNow() = LocalDateTime.now(ZoneId.of("Asia/Tokyo"))
     }
 
     private fun insertUpdate() {
-        val appId = System.getenv("APP_ID")!!
         val now = getNow()
         var cnt = 0
         for (i in 1..serviceConfiguration.nPage) {
             if (i != 1) {
                 Thread.sleep(1)
             }
-            val books = rakutenAPIService.listBooks(appId, i).execute().body()!!
-            val items = books.Items
-            for (elem in items) {
-                val item = elem.Item
-                if (item != null && isValidItem(item)) {
-                    val book = Book(
-                        id = null,
-                        title = item.title!!,
-                        author = item.author!!,
-                        itemUrl = item.itemUrl!!,
-                        imageUrl = item.mediumImageUrl!!,
-                        createdAt = now,
-                    )
-                    bookRepository.save(book)
-                    cnt += 1
-                }
-                if (cnt == serviceConfiguration.nMaxInsert) {
+            val bookDtos = rakutenAPIService.listBooks(i).filter { it.isNotComic() }.toList()
+            for (bookDto in bookDtos) {
+                val book = Book(
+                    id = null,
+                    bookDto.title,
+                    bookDto.author,
+                    bookDto.itemUrl,
+                    bookDto.imageUrl,
+                    createdAt = now,
+                )
+                bookRepository.save(book)
+                cnt += 1
+                if (cnt >= serviceConfiguration.nMaxInsert) {
                     return
                 }
             }
