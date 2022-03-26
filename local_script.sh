259d8109b0e68fd67609d2d82d7eda3da893b970
Jumpei Mori <nikutto.kom@gmail.com>
2022-03-26 09:57:52 +0900

    reconfig pghost

--- local_script.sh
+++ local_script.sh
@@ -2,7 +2,7 @@ echo "Processing..."
 
 set -e
 export PGDATABASE="testdb"
-export PGHOST="jdbc:postgresql://localhost:5432/${PGDATABASE}"
+export PGURL="jdbc:postgresql://localhost:5432/${PGDATABASE}"
 export PGUSER="postgres"
 export PGPASSWORD="password"
 set +e
